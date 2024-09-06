11f37387f48decd2c4a7a9d7c8cd94978669d636
HasithDeAlwis <hasithde24@gmail.com>
2024-09-06 17:59:57 -0400

    chore(repo): initialize shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config';
-import netlify from "@astrojs/netlify";
-
-import svelte from "@astrojs/svelte";
+import { defineConfig } from 'astro/config'
+import netlify from '@astrojs/netlify'
+import svelte from '@astrojs/svelte'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: 'server',
   adapter: netlify(),
-  integrations: [svelte()]
-});
+  integrations: [svelte(), tailwind({
+    applyBaseStyles: false,
+  })],
+})
