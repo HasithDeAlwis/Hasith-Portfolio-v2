bfa65f02ab4c4e1b2f0825ff6e562e3144158b51
HasithDeAlwis <hasithde24@gmail.com>
2024-09-07 19:24:17 -0400

    storybook(card/button): add example demos

---
+++ src/stories/index/card.stories.svelte
@@ -0,0 +1,36 @@
+<script lang="ts" context="module">
+  import type { Meta } from "@storybook/svelte";
+  import { Story, Template } from "@storybook/addon-svelte-csf";
+
+  import Card from "@components/index/card.svelte";
+
+  export const meta = {
+    title: "index/Card",
+    component: Card,
+    tags: ["autodocs"],
+    argTypes: {
+      title: {
+        control: { type: "text" },
+      },
+      body: {
+        control: { type: "text" },
+      },
+      href: {
+        control: { type: "text" },
+      },
+    },
+  } satisfies Meta<Card>;
+</script>
+
+<Template let:args>
+  <Card {...args} />
+</Template>
+
+<Story
+  name="Test"
+  args={{
+    title: "Test",
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    href: "",
+  }}
+/>
---
+++ src/stories/ui/button.stories.svelte
@@ -0,0 +1,51 @@
+<script lang="ts" context="module">
+  import type { Meta } from "@storybook/svelte";
+  import { Story, Template } from "@storybook/addon-svelte-csf";
+
+  import { Button } from "@shadcn/button";
+
+  export const meta = {
+    title: "UI/Button",
+    component: Button,
+    tags: ["autodocs"],
+    argTypes: {
+      variant: {
+        control: { type: "select" },
+        options: [
+          "default",
+          "destructive",
+          "outline",
+          "secondary",
+          "ghost",
+          "link",
+          undefined,
+        ],
+      },
+      size: {
+        control: { type: "select" },
+        options: ["default", "sm", "lg", "icon"],
+      },
+    },
+    parameters: {
+      actions: { disable: true },
+      controls: { disable: false },
+    },
+  } satisfies Meta<Button>;
+</script>
+
+<Template let:args>
+  <Button {...args}>{args.label || "Hello World"}</Button>
+</Template>
+
+<Story name="No args">
+  <Button>Hello</Button>
+</Story>
+
+<Story
+  name="Primary"
+  args={{ variant: "default", size: "lg", label: "Button" }}
+/>
+
+<Story name="Destructive" args={{ variant: "destructive", size: "sm" }} />
+
+<Story name="Secondary" args={{ variant: "secondary", size: "lg" }} />
