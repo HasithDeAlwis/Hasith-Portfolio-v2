d205b0f35d5d622c74fd95c1ff34754b0e114344
HasithDeAlwis <hasithde24@gmail.com>
2024-09-07 16:41:56 -0400

    chore(repo): configure storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,42 @@
+import path from 'node:path'
+import type { StorybookConfig } from '@storybook/svelte-vite'
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|ts|svelte)'],
+  addons: [
+    '@storybook/addon-svelte-csf',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/svelte-vite',
+    options: {},
+  },
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  async viteFinal(config) {
+    return {
+      ...config,
+      resolve: {
+        alias: [
+          {
+            find: '$lib',
+            replacement: path.resolve(__dirname, '../src'),
+          },
+          {
+            find: '@components',
+            replacement: path.resolve(__dirname, '../src/components'),
+          },
+          {
+            find: '@shadcn',
+            replacement: path.resolve(__dirname, '../src/components/ui'),
+          },
+        ],
+      },
+    }
+  },
+}
+export default config
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,7 @@ import { fontFamily } from 'tailwindcss/defaultTheme'
 /** @type {import('tailwindcss').Config} */
 const config = {
   darkMode: ['class'],
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './src/stories/**/*.{js,ts,svelte}'],
   safelist: ['dark'],
   theme: {
     container: {
