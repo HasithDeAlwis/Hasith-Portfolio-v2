ae312aad34fb3fd1f3e61efe4586b996ad61b7b0
HasithDeAlwis <hasithde24@gmail.com>
2024-10-04 12:48:52 -0400

    chore(website): scaffold basic about-me-text.svelte component

---
+++ apps/website/src/components/about-me/about-me-text.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let subheader = ''
+  export let description = ''
+</script>
+
+<div class='about-me'>
+  <h1>About Me</h1>
+  <p>
+    {subheader}
+  </p>
+  <p>
+    {description}
+  </p>
+</div>
