e9e58738536bd06e12c557319b078c3fdec1d6cf
HasithDeAlwis <hasithde24@gmail.com>
2024-10-04 16:14:07 -0400

    chore(website): create lazyimage svelte component
    This will be used to lazy load images in svelte components

---
+++ apps/website/src/components/ui/LazyImage/lazy-image.svelte
@@ -0,0 +1,14 @@
+<script lang='ts'>
+  export let src: string = ''
+  export let alt: string = ''
+  const srcset: string = ''
+  const sizes: string = ''
+</script>
+
+<img
+  src={src}
+  {srcset}
+  {sizes}
+  {alt}
+  loading='lazy'
+/>
