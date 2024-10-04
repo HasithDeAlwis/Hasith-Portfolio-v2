dad005bef1c0abe5afb5feb8812ec1086e2ec1f2
HasithDeAlwis <hasithde24@gmail.com>
2024-10-04 14:22:07 -0400

    ui(website/about-me-text): finish about-me-text component

--- apps/website/src/components/about-me/about-me-text.svelte
+++ apps/website/src/components/about-me/about-me-text.svelte
@@ -1,14 +1,22 @@
-<script>
-  export let subheader = ''
-  export let description = ''
+<script lang='ts'>
+  /**
+   * A subheader that appears underneath the larger "About Me"
+   * @type {string}
+   */
+  export let subheader: string = ''
+  /**
+   * A large piece of text that explains more about yourself to the client
+   * @type {string}
+   */
+  export let description: string = ''
 </script>
 
-<div class='about-me'>
-  <h1>About Me</h1>
-  <p>
+<div class='text-neutral-100'>
+  <h1 class='font-display text-display-3-mobile xl:text-display-3-desktop font-normal'>About Me</h1>
+  <p class='font-body text-poppins font-bold text-heading-4-mobile md:text-heading-3-mobile xl:text-heading-4-desktop'>
     {subheader}
   </p>
-  <p>
+  <p class='font-body text-p1-mobile md:text-p2-desktop xl:text-p1-desktop font-normal'>
     {description}
   </p>
 </div>
