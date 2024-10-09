3bcbe71be947afb5ac699e7e1d52eac267422408
HasithDeAlwis <hasithde24@gmail.com>
2024-10-09 01:00:39 -0400

    chore(website): update about-me-text.svelte for responsiveness

--- apps/website/src/components/about-me/about-me-text.svelte
+++ apps/website/src/components/about-me/about-me-text.svelte
@@ -12,11 +12,11 @@
 </script>
 
 <div class='text-neutral-100'>
-  <h1 class='font-display text-display-3-mobile xl:text-display-3-desktop font-normal'>About Me</h1>
+  <h1 class='font-display text-display-3-mobile text-display-1-mobile xl:text-display-3-desktop font-normal'>About Me</h1>
   <p class='font-body text-poppins font-bold text-heading-4-mobile md:text-heading-3-mobile xl:text-heading-4-desktop'>
     {subheader}
   </p>
-  <p class='font-body text-p1-mobile md:text-p2-desktop xl:text-p1-desktop font-normal'>
+  <p class='font-body text-p1-mobile md:text-p3-desktop xl:text-p1-desktop font-normal'>
     {description}
   </p>
 </div>
