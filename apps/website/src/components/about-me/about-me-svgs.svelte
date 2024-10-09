8d09e7d2162c0842b0470d99f801b532a7976bcd
HasithDeAlwis <hasithde24@gmail.com>
2024-10-09 01:01:12 -0400

    feat(website): create AboutMeSVGs component

---
+++ apps/website/src/components/about-me/about-me-svgs.svelte
@@ -0,0 +1,28 @@
+<script lang='ts'>
+  import type { AboutMeAssetType } from '@models/about-me'
+  import LazyImage from '@shadcn/LazyImage/lazy-image.svelte'
+  /**
+   * This number represents the vertical order the pictures appear in
+   * @type {number}
+   */
+  export let images: Array<AboutMeAssetType> = []
+
+  function getCSSFromOrder(order: number) {
+    switch (order) {
+      case 1: return 'bottom-0 left-[12em] rotate-[25deg] w-[100px] transform scale-x-[-1]'
+      case 2: return 'bottom-20 left-0 w-[150px]'
+      case 3: return 'top-0 left-[8em] w-[150px] rotate-[35deg] opacity-100'
+      case 4: return 'top-[-2em] left-[1em] w-[120px]'
+    }
+  }
+
+</script>
+
+<div class='w-[50px] h-[300px] xl:w-1/12 transform transfor scale-75 md:scale-[0.85] lg:scale-100'>
+  {#each images as image}
+    <div class={`absolute z-10 ${getCSSFromOrder(image.order)}`}>
+      <LazyImage src={image.svgURL ?? ''} alt={image.svgAlt ?? ''} />
+    </div>
+  {/each}
+
+</div>
