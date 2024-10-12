f4739338936bce62030e81363ae5111b272e3fc5
HasithDeAlwis <hasithde24@gmail.com>
2024-10-12 10:14:28 -0400

    chore(website/about-me-text): adjust id's

--- apps/website/src/components/about-me/about-me-text.svelte
+++ apps/website/src/components/about-me/about-me-text.svelte
@@ -49,16 +49,21 @@
     // -----ABOUT ME HEADER ANIMATIONS
     const splitHeader = new SplitType('#about-header', { types: 'chars' })
 
+    gsap.set(splitHeader, {
+      opacity: 0,
+      y: -100,
+    })
+
     // Animate each character
-    gsap.from(splitHeader.chars, {
+    gsap.to(splitHeader.chars, {
       scrollTrigger: {
         trigger: '#about-header', // Element to trigger the animation
         start: 'top 80%', // When the top of the element reaches 80% of the viewport
         end: 'top 70%', // When the top of the element reaches 50% of the viewport
         markers: true, // Optional for debugging
       },
-      opacity: 0, // Start with 0 opacity
-      y: -100, // Start 50px above the final position
+      opacity: 1, // Start with 0 opacity
+      y: 0, // Start 50px above the final position
       stagger: 0.08, // Stagger the animation by 0.05 seconds for a more fluid effect
       duration: 0.3,
       ease: 'power1.out', // Easing function
@@ -70,7 +75,7 @@
 
 </script>
 
-<div class='text-neutral-100'>
+<div id='about-me-description' class='text-neutral-100 about-me-description'>
   <h1 id='about-header' class='font-display text-display-3-mobile md:text-display-1-mobile xl:text-display-3-desktop font-normal'>About Me</h1>
   <p id='about-subheader' class='font-body text-poppins font-bold text-h4-mobile md:text-h3-mobile lg:text-h4-desktop'>
     {subheader}
