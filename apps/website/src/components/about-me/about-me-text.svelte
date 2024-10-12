68a6418cbc1802e9ad205b097ac90db48eba5835
HasithDeAlwis <hasithde24@gmail.com>
2024-10-12 09:10:48 -0400

    feat(website): add animations to about-me-text

--- apps/website/src/components/about-me/about-me-text.svelte
+++ apps/website/src/components/about-me/about-me-text.svelte
@@ -1,4 +1,9 @@
 <script lang='ts'>
+  import { gsap } from 'gsap'
+  import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+  import SplitType from 'split-type'
+  import { onMount } from 'svelte'
+
   /**
    * A subheader that appears underneath the larger "About Me"
    * @type {string}
@@ -9,14 +14,68 @@
    * @type {string}
    */
   export let description: string = ''
+  gsap.registerPlugin(ScrollTrigger)
+
+  function animateSubheader() {
+    gsap.to('#about-subheader', {
+      opacity: 1,
+      x: 0,
+      duration: 0.5,
+      onComplete: () => {
+        animateDescription()
+      },
+    })
+  }
+
+  function animateDescription() {
+    gsap.to('#about-description', {
+      opacity: 1,
+      y: 0,
+      duration: 0.5,
+    })
+  }
+
+  onMount(() => {
+    gsap.set('#about-subheader', {
+      opacity: 0,
+      x: -100,
+    })
+
+    gsap.set('#about-description', {
+      opacity: 0,
+      y: -100,
+    })
+
+    // -----ABOUT ME HEADER ANIMATIONS
+    const splitHeader = new SplitType('#about-header', { types: 'chars' })
+
+    // Animate each character
+    gsap.from(splitHeader.chars, {
+      scrollTrigger: {
+        trigger: '#about-header', // Element to trigger the animation
+        start: 'top 80%', // When the top of the element reaches 80% of the viewport
+        end: 'top 70%', // When the top of the element reaches 50% of the viewport
+        markers: true, // Optional for debugging
+      },
+      opacity: 0, // Start with 0 opacity
+      y: -100, // Start 50px above the final position
+      stagger: 0.08, // Stagger the animation by 0.05 seconds for a more fluid effect
+      duration: 0.3,
+      ease: 'power1.out', // Easing function
+      onComplete: () => {
+        animateSubheader()
+      },
+    })
+  })
+
 </script>
 
 <div class='text-neutral-100'>
-  <h1 class='font-display text-display-3-mobile text-display-1-mobile xl:text-display-3-desktop font-normal'>About Me</h1>
-  <p class='font-body text-poppins font-bold text-h4-mobile md:text-h3-mobile lg:text-h4-desktop'>
+  <h1 id='about-header' class='font-display text-display-3-mobile md:text-display-1-mobile xl:text-display-3-desktop font-normal'>About Me</h1>
+  <p id='about-subheader' class='font-body text-poppins font-bold text-h4-mobile md:text-h3-mobile lg:text-h4-desktop'>
     {subheader}
   </p>
-  <p class='font-body text-p1-mobile md:text-p3-desktop xl:text-p1-desktop font-normal'>
+  <p id='about-description'class='font-body text-p1-mobile md:text-p3-desktop xl:text-p1-desktop font-normal'>
     {description}
   </p>
 </div>
