eab6bc9bb391aa362495555b85bdead1bd896e50
HasithDeAlwis <hasithde24@gmail.com>
2024-10-12 19:06:36 -0400

    chore(website/api): modify API call

--- apps/website/src/api/about-me-api.ts
+++ apps/website/src/api/about-me-api.ts
@@ -3,36 +3,36 @@ import type { AboutMeAssetType, AboutMeTextType } from '$lib/models/about-me'
 import type { AboutMeAsset } from '$lib/models/generated-types'
 
 export async function getAboutMeData() {
-  const query
-   = `
+  const query = `
     query {
-        AboutMeAssets {
-            docs {
-            id
-            aboutMeOrder 
-            aboutMeSVG {
-                url
-                id
-                altText
-            }
-            }
+      AboutMeAssets {
+        docs {
+          aboutMeOrder
+          aboutMeSVG {
+            url
+            alt
+          }
+        } 
+      }
+      AboutMeDescriptions {
+        docs {
+          aboutMeDescription
+          aboutMeSubheader
         }
-        AboutMeDescriptions {
-            docs {
-            id
-            aboutMeSubheader
-            aboutMeDescription
-            }
-        }
+      }
     }
-    `
-  const endpoint = `${globalKeys.GRAPHQL_URL}/about-me-asset?query=${query}`
+`
+
+  const endpoint = `${globalKeys.GRAPHQL_URL}`
+
   try {
     const response = await fetch(endpoint, {
-      method: 'GET',
+      method: 'POST', // Changed to POST
       headers: {
         'Content-Type': 'application/json',
       },
+
+      body: JSON.stringify({ query }), // Send query in body
     })
 
     if (!response.ok) {
@@ -43,11 +43,12 @@ export async function getAboutMeData() {
     const assetData: Array<AboutMeAssetType> = data.data.AboutMeAssets.docs.map((asset: AboutMeAsset) => {
       return {
         id: asset.id,
-        svgURL: asset.aboutMeSVG.url,
-        svgAlt: asset.aboutMeSVG.altText,
+        svgURL: `http://localhost:3000${asset.aboutMeSVG.url}`,
+        svgAlt: asset.aboutMeSVG.alt,
         order: asset.aboutMeOrder,
       }
     })
+
     const textData: AboutMeTextType = data.data.AboutMeDescriptions.docs[0]
     return { assetData, textData }
   }
