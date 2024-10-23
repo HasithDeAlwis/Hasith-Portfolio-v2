834aad2b7ce5abe4bcc0ed9bead82f5aa3acd755
HasithDeAlwis <hasithde24@gmail.com>
2024-10-23 14:45:01 -0400

    feat(website): create footer and story

---
+++ apps/storybook/stories/base/footer.stories.svelte
@@ -0,0 +1,17 @@
+<script context='module'>
+  import Footer from '@components/base/footer.svelte'
+  import { Story, Template } from '@storybook/addon-svelte-csf'
+
+  export const meta = {
+    title: 'Base/Footer',
+    component: Footer,
+    tags: ['autodocs'],
+  }
+
+</script>
+
+<Template>
+  <Footer />
+</Template>
+
+<Story name='Default Footer' />
---
+++ apps/website/src/components/base/footer.svelte
@@ -0,0 +1,41 @@
+<script>
+  import GitHub from '@assets/base/github.svg?url'
+  import LinkedIn from '@assets/base/linkedin.svg?url'
+  import Mail from '@assets/base/mail.svg?url'
+  import Figma from '@assets/base/figma.svg?url'
+  import LazyImage from '@shadcn/LazyImage/lazy-image.svelte'
+
+  const socials = {
+    github: {
+      link: 'https://github.com/HasithDeAlwis',
+      picSrc: GitHub,
+    },
+    linkedin: {
+      link: 'https://www.linkedin.com/in/hasith-de-alwis/',
+      picSrc: LinkedIn,
+    },
+    email: {
+      link: 'mailto:hasithde24@gmail.com',
+      picSrc: Mail,
+    },
+    figma: {
+      link: 'https://www.figma.com/design/5mPcnmiJ9VuclwIzIPG8gp/Hasith-De-Alwis-Portfolio?node-id=103-1175&t=LMmUihIOsT3SFnPv-1',
+      picSrc: Figma,
+    },
+  }
+</script>
+
+<footer class='flex flex-col items-center p-4 bg-slate-300'>
+  <p class='text-primary-800'>
+    Made with ❤️ by Hasith
+  </p>
+  <div class='flex justify-center mt-2 space-x-4'>
+    {#each Object.entries(socials) as [key, { link, picSrc }]}
+      <div class='w-10 transition-transform hover:scale-110'>
+        <a href={link} target='_blank' rel='noopener noreferrer' class='text-gray-600 hover:text-primary-800'>
+          <LazyImage src={picSrc} alt={`${key} logo`} />
+        </a>
+      </div>
+    {/each}
+  </div>
+</footer>
