7229f6555d06498224ebe55e588dcaa699f71c9b
HasithDeAlwis <hasithde24@gmail.com>
2024-10-23 09:35:27 -0400

    chore(tailwind): adjust custom shadow classes

--- apps/website/src/components/skills/currently-learning.svelte
+++ apps/website/src/components/skills/currently-learning.svelte
@@ -15,20 +15,20 @@
   export let currentlyLearning: CurrentlyLearningSkillType[] = []
 // TODO: fix bug with the image scaling
 </script>
-<div id='repeated-skills' class=' bg-primary-100 min-h-screen'>
-  <div class='flex flex-col-reverse lg:flex-row justify-center pt-10'>
+<div id='repeated-skills' class='min-h-screen bg-primary-100'>
+  <div class='flex flex-col-reverse justify-center pt-10 lg:flex-row'>
     <div class='ml-4 mt-10 lg:mt-0 basis-full lg:basis-2/5 xl:basis-1/4 flex justify-center flex-wrap lg:max-h-[200px]'>
       {#each logos as _, index (index)}
-        <div class='w-1/3 p-2 flex justify-center transition-transform hover:scale-110'>
+        <div class='flex justify-center w-1/3 p-2 transition-transform hover:scale-110'>
           <div class={`current-logo-${index}`}>
 
           </div>
         </div>
       {/each}
     </div>
-    <div id='currently-learning-section' class='mt-3 lg:mt-0 lg:ml-3 basis-full lg:basis-1/2 xl:basis-2/3 p-2 flex justify-center'>
-      <div class='w-11/12 px-6 lg:p-10 flex flex-col items-center bg-primary-800 shadow-primary hover:shadow-primary-hover transition-shadow duration-300'>
-        <h2 class='text-large-1-mobile md:text-large-3-desktop xl:text-large-1-desktop font-bold text-center font-body text-neutral-100'>Currently Learning</h2>
+    <div id='currently-learning-section' class='flex justify-center p-2 mt-3 lg:mt-0 lg:ml-3 basis-full lg:basis-1/2 xl:basis-2/3'>
+      <div class='flex flex-col items-start w-11/12 px-6 transition-shadow duration-300 lg:p-10 bg-primary-800 shadow-primary-600 hover:shadow-primary-600-hover'>
+        <h2 class='font-bold text-center text-large-1-mobile md:text-large-3-desktop xl:text-large-1-desktop font-body text-neutral-100'>Currently Learning</h2>
         {#each currentlyLearning as skill, index (index)}
           <div>
             <IndividualSkill skill={skill} />
