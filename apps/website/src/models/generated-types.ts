298f7106f646d8d375776660ad9235089cf64219
HasithDeAlwis <hasithde24@gmail.com>
2024-10-22 23:00:23 -0400

    chore(models): update models to include the skills section

--- apps/website/src/models/generated-types.ts
+++ apps/website/src/models/generated-types.ts
@@ -14,6 +14,8 @@ export interface Config {
     'media': Media
     'about-me-asset': AboutMeAsset
     'about-me-description': AboutMeDescription
+    'learned-skill-logos': LearnedSkillLogo
+    'currently-learning-skill': CurrentlyLearningSkill
     'payload-locked-documents': PayloadLockedDocument
     'payload-preferences': PayloadPreference
     'payload-migrations': PayloadMigration
@@ -105,6 +107,31 @@ export interface AboutMeDescription {
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "learned-skill-logos".
+ */
+export interface LearnedSkillLogo {
+  id: number
+  skillOrder: number
+  skillLogo: Media
+  skillHref: string
+  updatedAt: string
+  createdAt: string
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "currently-learning-skill".
+ */
+export interface CurrentlyLearningSkill {
+  id: number
+  currentSkillName: string
+  currentSkillLogo: Media
+  currentSkillHref: string
+  currentSkillDescription: string
+  updatedAt: string
+  createdAt: string
+}
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
  * via the `definition` "payload-locked-documents".
  */
 export interface PayloadLockedDocument {
@@ -116,7 +143,7 @@ export interface PayloadLockedDocument {
     } | null)
     | ({
       relationTo: 'media'
-      value: number | Media
+      value: Media
     } | null)
     | ({
       relationTo: 'about-me-asset'
@@ -125,6 +152,14 @@ export interface PayloadLockedDocument {
     | ({
       relationTo: 'about-me-description'
       value: number | AboutMeDescription
+    } | null)
+    | ({
+      relationTo: 'learned-skill-logos'
+      value: number | LearnedSkillLogo
+    } | null)
+    | ({
+      relationTo: 'currently-learning-skill'
+      value: number | CurrentlyLearningSkill
     } | null)
   globalSlug?: string | null
   user: {
---
+++ apps/website/src/models/skills.ts
@@ -0,0 +1,13 @@
+export interface LearnedSkillLogoType {
+  url: string
+  alt: string
+  link: string
+}
+
+export interface CurrentlyLearningSkillType {
+  picUrl: string
+  alt: string
+  link: string
+  description: string
+  name: string
+}
