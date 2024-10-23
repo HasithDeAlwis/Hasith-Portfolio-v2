1e5a417f98e5b3ba6125a5d18c8db3eb4fd63d27
HasithDeAlwis <hasithde24@gmail.com>
2024-10-22 23:02:37 -0400

    chore(storybook): create story for all skills components

---
+++ apps/storybook/stories/skills/individual-skill.stories.svelte
@@ -0,0 +1,36 @@
+<script context='module'>
+  import IndividualSkill from '@components/skills/individual-skill.svelte'
+  import { Story, Template } from '@storybook/addon-svelte-csf'
+  import RubyLogo from './../../assets/Ruby.svg'
+  import AWS from './../../assets/aws.svg'
+
+  export const meta = {
+    title: 'Skills/Individual-Skills',
+    component: IndividualSkill,
+    tags: ['autodocs'],
+  }
+
+  const skill1 = {
+    picUrl: RubyLogo,
+    alt: 'Ruby Logo',
+    description: 'Ruby is a language I\'m currently learning and exploring to gaing new experiences with BE technologies. It\'s been very fun so far and I\'m almost done with the basics and moving forward to building my next personal project with it.',
+    name: 'Ruby',
+    link: 'https://www.ruby-lang.org/en/',
+  }
+
+  const skill2 = {
+    picUrl: AWS,
+    alt: 'AWS Logo',
+    description: 'AWS is a cloud service I\'m currently learning and exploring to gaing new experiences with cloud technologies. It\'s been very fun so far and I\'m almost done with the basics and moving forward to building my next personal project with it.',
+    name: 'AWS',
+    link: 'https://aws.amazon.com/',
+  }
+
+</script>
+
+<Template let:args>
+  <IndividualSkill {...args} />
+</Template>
+
+<Story name='Ruby Skill' args={{ skill: skill1 }} />
+<Story name='AWS Skill' args={{ skill: skill2 }} />
