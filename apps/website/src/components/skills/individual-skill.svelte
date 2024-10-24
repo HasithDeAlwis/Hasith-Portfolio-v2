8fed5431d93830142fc9b58b46c17d664fe7a7b4
HasithDeAlwis <hasithde24@gmail.com>
2024-10-23 20:19:14 -0400

    ui(website/currently-learning): align text left and improve spacing

--- apps/website/src/components/skills/individual-skill.svelte
+++ apps/website/src/components/skills/individual-skill.svelte
@@ -15,18 +15,18 @@
   }
 </script>
 
-<div class='py-2'>
+<div class='py-4'>
   <div class='mb-[-1]'>
-    <h1 class='text-neutral-100 text-h3-mobile md:text-h4-desktop lg:text-h3-desktop font-bold font-body'>{skill.name}</h1>
+    <h1 class='font-bold text-neutral-100 text-h3-mobile md:text-h4-desktop lg:text-h3-desktop font-body'>{skill.name}</h1>
   </div>
-  <div class='flex justify-start items-center'>
+  <div class='flex items-center justify-start'>
     <div class=''>
       <a href={skill.link} target='_blank' rel='noopener noreferrer'>
         <LazyImage src={skill.picUrl} alt={skill.alt} classStyles='h-[70px] 3xl:h-[100px] object-contain transition-transform hover:scale-110' />
       </a>
     </div>
 
-    <div class='basis-4/5 pl-6 '>
+    <div class='pl-6 basis-4/5 '>
       <p class='text-neutral-100 text-p1-mobile md:text-p2-desktop lg:text-p1-desktop'>{skill.description}</p>
     </div>
   </div>
