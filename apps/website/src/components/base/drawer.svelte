ee642448ab87fcfaecc5bf703faa19fbc391e94e
HasithDeAlwis <hasithde24@gmail.com>
2024-11-09 10:28:12 -0500

    fix(ui/web/navbar): navigation links not working outside home page

--- apps/website/src/components/base/drawer.svelte
+++ apps/website/src/components/base/drawer.svelte
@@ -1,56 +1,58 @@
-<script>
-  import CloseIcon from '@assets/base/close.svg?url'
-  import LazyImage from '@shadcn/LazyImage/lazy-image.svelte'
-  import { onDestroy } from 'svelte'
+<script lang="ts">
+  import CloseIcon from "@assets/base/close.svg?url";
+  import LazyImage from "@shadcn/LazyImage/lazy-image.svelte";
+  import { onDestroy } from "svelte";
 
-  export let isOpen = false
-  export let closeDrawer
-  export let links = []
+  export let isOpen = false;
+  export let closeDrawer;
+  export let links: { href: string; text: string; isHome: boolean }[] = [];
 
   // Prevent scrolling when the drawer is open
   $: if (isOpen) {
-    document.body.style.overflow = 'hidden'
-  }
-  else {
-    document.body.style.overflow = 'auto'
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "auto";
   }
   // Cleanup on component destroy
   onDestroy(() => {
-    document.body.style.overflow = 'auto' // Reset overflow style
-  })
+    document.body.style.overflow = "auto"; // Reset overflow style
+  });
 </script>
 
 <button
-  class={`fixed inset-0 bg-black bg-opacity-50 transition-opacity ${isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}
+  class={`fixed inset-0 bg-black bg-opacity-50 transition-opacity ${isOpen ? "opacity-100" : "opacity-0 pointer-events-none"}`}
   on:click={closeDrawer}
 ></button>
 
 <nav
   class={`fixed right-0 top-0 h-full w-64 bg-neutral-100 transform transition-transform duration-300 ease-in-out z-50 ${
-    isOpen ? 'translate-x-0' : 'translate-x-full'
+    isOpen ? "translate-x-0" : "translate-x-full"
   }`}
-  id='drawer'
+  id="drawer"
 >
-  <div class='w-16'>
+  <div class="w-16">
     <button
-      class={`p-4 transform transition-transform duration-400 ${isOpen ? 'rotate-180' : ''}`}
+      class={`p-4 transform transition-transform duration-400 ${isOpen ? "rotate-180" : ""}`}
       on:click={closeDrawer}
     >
-      <LazyImage src={CloseIcon} alt='close icon' />
+      <LazyImage src={CloseIcon} alt="close icon" />
     </button>
   </div>
-  <ul class='flex flex-col p-4'>
-    {#each links as { href, text }}
-      <li class='py-2'>
-        <a href={href} class='text-primary-600 hover:text-primary-800'>{text}</a>
+  <ul class="flex flex-col p-4">
+    {#each links as { href, text, isHome }}
+      <li class="py-2">
+        <a
+          href={window.location.pathname !== "/" && isHome ? `/${href}` : href}
+          class="text-primary-600 hover:text-primary-800">{text}</a
+        >
       </li>
     {/each}
   </ul>
 </nav>
 
 <style>
-    /* This class will rotate the icon by 180 degrees */
-    .is-rotated {
-      rotate: 180deg; /* Rotates the icon */
-    }
-  </style>
+  /* This class will rotate the icon by 180 degrees */
+  .is-rotated {
+    rotate: 180deg; /* Rotates the icon */
+  }
+</style>
--- apps/website/src/components/base/navbar.svelte
+++ apps/website/src/components/base/navbar.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang='ts'>
   import Logo from '@assets/hasith-logo.svg?url'
   import LazyImage from '@shadcn/LazyImage/lazy-image.svelte'
   import Drawer from '@components/base/drawer.svelte'
@@ -13,16 +13,19 @@
 
   // TODO: change colour of the navbar links when scrolling
   onMount(() => {
-    gsap.from('#header', {
-      scrollTrigger: {
-        trigger: '#hero',
-        start: 'top top',
-        end: '+=200',
-        scrub: true,
-      },
-      backgroundColor: 'transparent',
-    })
+    if (window.location.pathname === '/') {
+      gsap.from('#header', {
+        scrollTrigger: {
+          trigger: '#navbar',
+          start: 'top top',
+          end: '+=200',
+          scrub: true,
+        },
+        backgroundColor: 'transparent',
+      })
+    }
   })
+
   function toggleDrawer() {
     isDrawerOpen = !isDrawerOpen
   }
@@ -31,28 +34,35 @@
     isDrawerOpen = false
   }
 
+  // TODO THESE SHOULD BE PROPS WE PASS INTO THE NAVBAR BUTTONS COMPONENT
   const links = [
-    { href: '#hero', text: 'Home' },
-    { href: '#about', text: 'About' },
-    { href: '#my-skills', text: 'Skills' },
-    { href: '#projects', text: 'Projects' },
+    { href: '#hero', text: 'Home', isHome: true },
+    { href: '#about', text: 'About', isHome: true },
+    { href: '#my-skills', text: 'Skills', isHome: true },
+    { href: '#projects', text: 'Projects', isHome: true },
+    { href: '/blogs', text: 'Blogs', isHome: false },
   ]
+
+/* TODO REFACTOR SO THAT THE NAVBAR BUTTONS ARE COMPONENTS */
+
 </script>
 
-<header id='header' class='fixed top-0 left-0 right-0 z-40 bg-opacity-75 bg-primary-400 backdrop-blur-md'>
+<header id='header' class={`fixed top-0 left-0 right-0 z-40 bg-opacity-75  backdrop-blur-md ${window.location.pathname === '/' ? 'bg-primary-400' : 'bg-primary-100'}`}>
   <div class='flex items-center justify-between w-full px-6 py-4 mx-auto'>
     <div class='flex justify-start basis-1/2'>
-      <a href={window.location.pathname === '/' ? '#hero' : '/'} aria-label='Navigate to Home'>
-        <div class='w-[40%] lg:w-[12%] text-lg font-bold text-gray-800 transition-transform hover:scale-110'>
+      <div class='w-[40%] lg:w-[12%] text-lg font-bold text-gray-800 transition-transform hover:scale-110'>
+        <a href={window.location.pathname === '/' ? '#hero' : '/'} aria-label='Navigate to Home'>
+
           <LazyImage src={Logo} alt='Hasith De Alwis logo' classStyles='main-logo' />
-        </div>
-      </a>
+        </a>
+
+      </div>
     </div>
 
     <nav id='navbar' class='sm:basis-1/2 lg:basis-1/3'>
       <div class='items-center justify-around hidden space-x-4 md:flex'>
-        {#each links as { href, text }}
-          <a href={href} class='font-bold text-primary-800 hover:text-primary-900 focus:text-secondary-800 font-body text-p1-mobile md:text-p2-desktop'>
+        {#each links as { href, text, isHome }}
+          <a aria-label={`navigate to ${text} section`} href={window.location.pathname !== '/' && isHome ? `/${href}` : href} class='font-bold text-primary-800 hover:text-primary-900 focus:text-secondary-800 font-body text-p1-mobile md:text-p2-desktop'>
             {text}
           </a>
         {/each}
